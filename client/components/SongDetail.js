ce8eab75e0e095c2525aff0ca2eebb71f1d22e4c
kmuganda <chateauindigo@gmail.com>
2018-07-14 17:29:38 -0400

    initial

---
+++ client/components/SongDetail.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import FetchSong from '../queries/FetchSong';
+import { Link } from 'react-router';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
+
+class SongDetail extends Component {
+    render() {
+        const { song } = this.props.data;
+        if (!song) { return <div>Loading...</div>; }
+
+        return (
+            <div>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
+                <LyricList lyrics={song.lyrics} />
+                <LyricCreate songId={this.props.params.id}/>
+            </div>
+        );
+    }
+}
+
+export default graphql(FetchSong, {
+    options: (props) => { return { variables: { id: props.params.id }}}
+})(SongDetail);
